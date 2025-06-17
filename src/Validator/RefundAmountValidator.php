600a53217217f25730b4ae0566ebb3046f3ac5d4
Dario Di Bella <dario@originem.io>
2025-06-17 16:25:51 +0200

    Remove wrong selection of RefundType

--- src/Validator/RefundAmountValidator.php
+++ src/Validator/RefundAmountValidator.php
@@ -29,15 +29,6 @@ final class RefundAmountValidator implements RefundAmountValidatorInterface
 
     public function validateUnits(array $unitRefunds): void
     {
-        $args = func_get_args();
-        $refundType = null;
-
-        if (isset($args[1])) {
-            trigger_deprecation('sylius/refund-plugin', '1.4', sprintf('Passing a 2nd argument of "%s::validateUnits" method is deprecated and will be removed in 2.0.', self::class));
-
-            $refundType = $args[1];
-        }
-
         Assert::allIsInstanceOf($unitRefunds, UnitRefundInterface::class);
 
         /** @var UnitRefundInterface $unitRefund */
@@ -50,7 +41,7 @@ final class RefundAmountValidator implements RefundAmountValidatorInterface
 
             $unitRefundedTotal = $this->remainingTotalProvider->getTotalLeftToRefund(
                 $unitRefund->id(),
-                null === $refundType ? $unitRefund->type() : $refundType,
+                 $unitRefund->type(),
             );
 
             if ($unitRefund->total() > $unitRefundedTotal) {
